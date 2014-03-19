7fe56139eba240ef4005261e8bd0b5bf7fde4208
Mathew Chasan <mathew@thebigincredible.com>
2014-03-18 18:53:16 -0700

    Made all print functions virtual
     - surprising note.  This commit close to doubles the size of a program
     that prints a single string.
    
     - EXAMPLE:
      with commit: Binary sketch size: 6,462 bytes (of a 30,720 byte maximum)
      w/o  commit: Binary sketch size: 3,314 bytes (of a 30,720 byte maximum)

--- hardware/bean/cores/bean/Print.h
+++ hardware/bean/cores/bean/Print.h
@@ -52,30 +52,30 @@ class Print
     }
     virtual size_t write(const uint8_t *buffer, size_t size);
     
-    size_t print(const __FlashStringHelper *);
-    size_t print(const String &);
-    size_t print(const char[]);
-    size_t print(char);
-    size_t print(unsigned char, int = DEC);
-    size_t print(int, int = DEC);
-    size_t print(unsigned int, int = DEC);
-    size_t print(long, int = DEC);
-    size_t print(unsigned long, int = DEC);
-    size_t print(double, int = 2);
-    size_t print(const Printable&);
+    virtual size_t print(const __FlashStringHelper *);
+    virtual size_t print(const String &);
+    virtual size_t print(const char[]);
+    virtual size_t print(char);
+    virtual size_t print(unsigned char, int = DEC);
+    virtual size_t print(int, int = DEC);
+    virtual size_t print(unsigned int, int = DEC);
+    virtual size_t print(long, int = DEC);
+    virtual size_t print(unsigned long, int = DEC);
+    virtual size_t print(double, int = 2);
+    virtual size_t print(const Printable&);
 
-    size_t println(const __FlashStringHelper *);
-    size_t println(const String &s);
-    size_t println(const char[]);
-    size_t println(char);
-    size_t println(unsigned char, int = DEC);
-    size_t println(int, int = DEC);
-    size_t println(unsigned int, int = DEC);
-    size_t println(long, int = DEC);
-    size_t println(unsigned long, int = DEC);
-    size_t println(double, int = 2);
-    size_t println(const Printable&);
-    size_t println(void);
+    virtual size_t println(const __FlashStringHelper *);
+    virtual size_t println(const String &s);
+    virtual size_t println(const char[]);
+    virtual size_t println(char);
+    virtual size_t println(unsigned char, int = DEC);
+    virtual size_t println(int, int = DEC);
+    virtual size_t println(unsigned int, int = DEC);
+    virtual size_t println(long, int = DEC);
+    virtual size_t println(unsigned long, int = DEC);
+    virtual size_t println(double, int = 2);
+    virtual size_t println(const Printable&);
+    virtual size_t println(void);
 };
 
 #endif

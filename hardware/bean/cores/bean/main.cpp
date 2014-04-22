ce2b6d55f333f8e7e6cde1908021ff3879672392
Mathew Chasan <mathew@thebigincredible.com>
2014-04-22 11:53:03 -0700

    Changed main.cpp tabs-->spaces

--- hardware/bean/cores/bean/main.cpp
+++ hardware/bean/cores/bean/main.cpp
@@ -2,22 +2,22 @@
 
 int main(void)
 {
-	init();
+  init();
 
 #if defined(USBCON)
-	USBDevice.attach();
+  USBDevice.attach();
 #endif
   // Ensure that BeanSerialTransport.begin() is called for control messages
   // even if users are not using the serial port.
   // A user calling this again shouldn't cause any harm.
   Serial.begin();
-	setup();
+  setup();
 
-	for (;;) {
-		loop();
-		if (serialEventRun) serialEventRun();
-	}
+  for (;;) {
+    loop();
+    if (serialEventRun) serialEventRun();
+  }
         
-	return 0;
+  return 0;
 }
 

9ad0bb7b3d336c7e37343f46cfbbaa0f0c45613e
Matthew Lewis <matt@mplewis.com>
2015-09-25 16:30:14 -0500

    deploy: add gulp deploy to GH-Pages task

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp')
+var ghPages = require('gulp-gh-pages')
+var add = require('gulp-add')
+var exec = require('child_process').exec
+
+var config = {
+  push: true
+}
+
+function deployMarkers() {
+  return {
+    'UPDATED_AT': new Date().toISOString()
+  }
+}
+
+gulp.task('build', function(done) {
+  exec('make docs', function(err, stdout, stderr) {
+    if (err) throw err
+    process.stdout.write(stdout)
+    process.stderr.write(stderr)
+    done()
+  })
+})
+
+gulp.task('deploy', ['build'], function() {
+  return gulp.src('docs/build/**/*')
+    .pipe(add(deployMarkers()))
+    .pipe(ghPages(config))
+})

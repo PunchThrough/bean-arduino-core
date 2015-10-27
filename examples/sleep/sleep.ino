f97087c02dc4dd54810dd4d1288ff540d7bc99ba
Matthew Lewis <matt@mplewis.com>
2015-10-27 15:22:36 -0500

    docs: make sleep example consistent with others

--- examples/sleep/sleep.ino
+++ examples/sleep/sleep.ino
@@ -6,11 +6,11 @@ void loop() {
   // Check if LED is on
   if (Bean.getLedGreen() == 0) {
     // LED is off. Turn LED on and sleep for 1 second
-    Bean.setLedGreen(255);
+    Bean.setLed(0, 255, 0);
     Bean.sleep(1000);
   } else {
     // LED is on. Turn LED off and sleep for 4 seconds
-    Bean.setLedGreen(0);
+    Bean.setLed(0, 255, 0);
     Bean.sleep(4000);
   }
 }

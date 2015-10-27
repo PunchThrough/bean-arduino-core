000b2740075cade63f81e19d9adf3d6a0ae6abd4
Matthew Lewis <matt@mplewis.com>
2015-10-27 15:21:11 -0500

    docs: improve enableWakeOnConnect.ino

--- examples/sleep/enableWakeOnConnect.ino
+++ examples/sleep/enableWakeOnConnect.ino
@@ -3,17 +3,27 @@ void setup() {
   Bean.enableWakeOnConnect(true);
 }
 
-// Loop runs once when a client connects and continues running as long as a
-// client is connected
+// Loop runs once at program start.
+// When a client connects, Bean wakes up and runs loop again.
+// Loop continues to run as long as a client is connected.
+
 void loop() {
   // Check if client has disconnected
   bool connected = Bean.getConnectionState();
+
   if (connected) {
     // Client connected: set LED to green
     Bean.setLed(0, 255, 0);
+
   } else {
-    // Client disconnected: turn off LED and sleep for a long time
+    // Client disconnected: turn off LED
     Bean.setLed(0, 0, 0);
+
+    // This sketch runs only when a client is connected.
+    // This might be useful for interactive installations, where you don't want
+    // Bean to spend any power unless someone is actively connected.
+
+    // Sleep for a long time to conserve power.
     // 0xFFFFFFFF = 4,294,967,295 milliseconds = 49 days
     Bean.sleep(0xFFFFFFFF);
   }

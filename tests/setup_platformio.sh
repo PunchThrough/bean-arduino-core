61fc98fcd6f029b9417b79fba05b4a2e373a817e
Matthew Lewis <matt@mplewis.com>
2016-01-04 13:58:03 -0600

    ci: fix setup platformio script to create dirs

--- tests/setup_platformio.sh
+++ tests/setup_platformio.sh
@@ -3,6 +3,14 @@
 # Installing PlatformIO with pip is kind of broken. Just use their install script.
 python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
 platformio platforms install atmelavr
-mkdir -p ~/.platformio/boards  # -p: no error if cached /boards already exists
+
+# Add Punch Through boards definition
+# -p: no error if cached /boards already exists
+mkdir -p ~/.platformio/boards
 cp tests/resources/platformio/boards/punchthrough.json ~/.platformio/boards/
+
+# Add Punch Through core files
+# packages/framework-arduinoavr may not exist until the first compile happens,
+# so create the directory right away
+mkdir -p /home/ubuntu/.platformio/packages/framework-arduinoavr/
 cp -rvf hardware/bean/avr/* ~/.platformio/packages/framework-arduinoavr/
